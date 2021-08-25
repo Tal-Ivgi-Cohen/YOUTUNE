b1d2826fa88377f5f8d1e82e723c064b99ab5169
Tal-Ivgi-Cohen <tal.ivgi@gmail.com>
2021-08-25 10:53:55 +0300

    scss 2

--- Frontend/src/pages/SingerProfile.jsx
+++ Frontend/src/pages/SingerProfile.jsx
@@ -4,7 +4,6 @@ import { loadSongs } from '../store/song/song.action.js';
 import { loadUsers } from '../store/user/user.action.js';
 import { useDispatch, useSelector } from 'react-redux'
 
-
 export const SingerProfile = () => {
   const dispatch = useDispatch()
   const { selectedSong } = useSelector(state => state.songModule)
@@ -21,31 +20,31 @@ export const SingerProfile = () => {
   }, [])
 
   const findSingerUser = () => {
-    const  id  = selectedSong.singer._id
+    const id = selectedSong.singer._id
     const user = users.find((user) => user._id === id)
     return user
 
   }
   const userSinger = findSingerUser()
 
-  
+
   return (
     <Fragment>
       <div className="contianer-hero">
         <img src={userSinger.imgHero} alt="" className="img-hero-artist" />
-        <div className="singer-hero-title">
+        <div className="artist-hero-title">
           <p>{userSinger.specializes}</p>
           <h1>{userSinger.fullname}</h1>
         </div>
       </div>
-      <div className="main-singer">
-
-        <div className="singer-details flex">
+      <div className="main-artist">
+        <div className="artist-details flex">
           <img src={userSinger.imgUrl} alt={userSinger.fullname} />
-          <div className="text-description flex column align-center">
-            <h4>About {userSinger.fullname}</h4>
+        </div>
+        <div className="text-description flex column align-center">
+          <h4>About {userSinger.fullname}</h4>
+          <div className="singer-desc">
             <p><LongTxt description={userSinger.description} /></p>
-            <br />
           </div>
         </div>
       </div>
